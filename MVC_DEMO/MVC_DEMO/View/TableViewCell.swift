1d20f5c2b5ac550de86f6dee0a2411381879a544
FETC_iMac2 <TommySTsai@fetc.net.tw>
2022-05-16 16:34:04 +0800

    mvc 程式架構

---
+++ MVC_DEMO/MVC_DEMO/View/TableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  TableViewCell.swift
+//  MVC_DEMO
+//
+//  Created by FETC_iMac2 on 2022/5/16.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titlelb: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MVC_DEMO/MVC_DEMO/ViewController/AddViewController.swift
@@ -0,0 +1,60 @@
+//
+//  AddViewController.swift
+//  MVC_DEMO
+//
+//  Created by FETC_iMac2 on 2022/5/16.
+//
+
+import UIKit
+
+protocol AddViewControllerDelegate {
+    func reloadTableView()
+}
+
+class AddViewController: UIViewController {
+
+    @IBOutlet weak var titletf: UITextField!
+    @IBOutlet weak var contenttf: UITextField!
+
+    var delegate: AddViewControllerDelegate?
+    var modelManager: ModelManager? = nil
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func clickSave(_ sender: Any) {
+
+        if titletf.text!.isEmpty || contenttf.text!.isEmpty {
+            showAlert(title: "請輸入標題/內文")
+            return
+        }
+
+        let model = Model(title: titletf.text!, detail: Model.Detail(detailText: contenttf.text!))
+
+        modelManager?.model.append(model)
+        delegate?.reloadTableView()
+        navigationController?.popViewController(animated: true)
+    }
+
+    func showAlert(title: String){
+
+        let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
+
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
+    }
+
+}
